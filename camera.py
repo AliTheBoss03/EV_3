41cf4bd5635587525d4b44e6b07a8396f1217960
Ali Masoud <alimohamadbro@gmail.com>
2024-06-17 12:01:42 +0200

    x

--- camera.py
+++ camera.py
@@ -1,7 +1,7 @@
 import cv2
 import requests
 
-FLASK_SERVER_URL = "http://172.20.10.4:5000/process_image"  # Brug den korrekte IP-adresse til din server
+FLASK_SERVER_URL = "http://172.20.10.6:5000/process_image"  # Brug den korrekte IP-adresse til din server
 
 def capture_and_send_image():
     cap = cv2.VideoCapture(0)  # Vælg den korrekte kameraenhed
